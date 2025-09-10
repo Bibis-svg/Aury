6e3b408fe59adde89320ae074ecafad27fec5ee2
Bia <biarbcosta17@gmail.com>
2025-09-10 20:55:19 -0300

    app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/cadastro" replace />} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/home" element={<Home />} />
       </Routes>
